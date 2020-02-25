0e5e62e80f85c3fac36a4f89a2a73525a901f15d
Vladimir Parrales <vladimirparrales@Vladimirs-MacBook-Pro.local>
2020-02-24 18:05:00 -0800

    worked on styling the contact form

---
+++ practice/javascript/week_6/first.js
@@ -0,0 +1,32 @@
+console.log(10 * 10);
+console.log(1000);
+console.log(1500 / 3);
+console.log('Vladimir');
+console.log('100');
+console.log('hello' * 10);
+
+// You cannot do math on strings
+console.log('hello' * "bye");
+console.log(100 / 10 * 5);
+
+// Order of operations
+console.log(100 / (10 * 5));
+
+// This is not math, the + can be used for addition OR concatination:
+console.log('How are you ' + 'Vladimir');
+
+console.log('Hello, "how" are you?');
+
+// The Boolean dataype 
+console.log(true);
+console.log(false);
+
+console.log(1);
+console.log(0);
+
+//NaN is a number
+NaN;
+
+console.log(100 / 0);
+
+console.log(NaN);
