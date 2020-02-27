c667f27234c86a95cf16cdd0013debd891fc38a1
Vladimir Parrales <vladimirparrales@Vladimirs-MacBook-Pro.local>
2020-02-27 12:56:55 -0800

    introduction to if statements.

---
+++ practice/javascript/week_6/ifstatements.js
@@ -0,0 +1,37 @@
+// If-Else Statements, conditional statements
+
+// if (condition) {}
+
+if (false) {
+    console.log("This code ran if the if statement was true.");
+}
+
+console.log("End of script");
+
+// Comparison operators or "comparitors"
+
+// == equal to
+// >= greater than or equal
+// > greater than
+// <= less than or equal
+// < less than
+// != not equal (should not equal to be true)
+
+ var age = 22;
+
+ if (age >= 21) {
+     console.log("Congratz, go get drunk!");
+ }
+
+
+ var password = "abcdef";
+
+ if (password == "abcdef") {
+    console.log("Welcome to your profile!");
+ }
+
+ var isSleepy = true;
+
+ if (isSleepy) {
+     console.log("go home and sleep");
+ }
